05239bfe49811ade13256a6d639a702885d662d7
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-22 17:17:56 -0400

    edit button goes thru

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,6 +1,6 @@
 class WorldsController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_world, except: [:index, :new, :create, :show]
+    before_action :set_world, except: [:index, :new, :create, :show, :destroy]
 
     def index
         @worlds = World.all
