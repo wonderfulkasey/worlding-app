213fd4016f6e4f3608f4c615ac14d14c3b1a921f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-22 01:25:08 -0400

    added scope to world

--- app/models/world.rb
+++ app/models/world.rb
@@ -1,8 +1,15 @@
 class World < ApplicationRecord
     belongs_to :user
-    has_many :characters
-    has_many :plots
+        #when delete world, deletes chars and plots
+    has_many :characters, :dependent => :destroy
+    has_many :plots, :dependent => :destroy
 
     validates :name, presence: true
-    validates :genre, presence: true 
+    #validates :genre, presence: true 
+
+    #where you world by character count
+    def self.most_characters(user)
+        where(completed: true, user_id: user.id).count
+    end
+
 end
