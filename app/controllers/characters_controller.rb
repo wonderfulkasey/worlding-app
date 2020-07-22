56938b1be367d98b0ad0d328b335378127f9434a
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-22 02:03:04 -0400

    finished char and plot cont

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,30 +3,67 @@ class CharactersController < ApplicationController
     before_action :set_character, except: [:index, :new, :create]
 
     def index
+        @characters = Character.all
     end
 
     def show
+        #@character = Character.find(params[:id])
+        @world = @character.world
     end
 
     def new
+        if params[:world_id] && !World.exists?(params[:world_id])
+            redirect_to worlds_path, alert: "World not found."
+        else
+             @character = Character.new(world_id: params[:world_id])
+        end
     end
 
     def create
+        @character = Character.new(character_params)
+        @character.user_id = current_user.id
+          
+          if @character.valid?
+              @character.save
+              redirect_to character_path(@character)
+          else
+            flash[:errors] = @character.errors.full_messages
+            render :new
+          end
     end
 
     def edit
+        if params[:world_id]
+            w = World.find_by(id: params[:world_id])
+            if w.nil?
+              redirect_to worlds_path, alert: "World not found."
+            else
+              @character = w.characters.find_by(id: params[:id])
+              redirect_to world_characters_path(w), alert: "character not found." if @character.nil?
+            end
+        end
     end
 
     def update
+        @character.update(character_params)
+        
+        if @character.save  
+            redirect_to @character
+        else
+            flash[:errors] = @character.errors.full_messages
+            render :edit
+        end
     end
 
     def destroy
+        @character.destroy
+        redirect_to characters_path 
     end
 
     private 
 
-    def world_params
-        params.require(:world).permit(
+    def character_params
+        params.require(:character).permit(
             :name
             :alignment
             :species
--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -3,30 +3,67 @@ class PlotsController < ApplicationController
     before_action :set_plot, except: [:index, :new, :create]
 
     def index
+        @plots = Plot.all
     end
 
     def show
+        #@plot = plot.find(params[:id])
+        @world = @plot.world
     end
 
     def new
+        if params[:world_id] && !World.exists?(params[:world_id])
+            redirect_to worlds_path, alert: "World not found."
+        else
+             @plot = Plot.new(world_id: params[:world_id])
+        end
     end
 
     def create
+        @plot = Plot.new(plot_params)
+        @plot.user_id = current_user.id
+          
+          if @plot.valid?
+              @plot.save
+              redirect_to plot_path(@plot)
+          else
+            flash[:errors] = @plot.errors.full_messages
+            render :new
+          end
     end
 
     def edit
+        if params[:world_id]
+            w = World.find_by(id: params[:world_id])
+            if w.nil?
+              redirect_to worlds_path, alert: "World not found."
+            else
+              @plot = w.plots.find_by(id: params[:id])
+              redirect_to world_plots_path(w), alert: "Plot not found." if @plot.nil?
+            end
+        end
     end
 
     def update
+        @plot.update(plot_params)
+        
+        if @plot.save  
+            redirect_to @plot
+        else
+            flash[:errors] = @plot.errors.full_messages
+            render :edit
+        end
     end
 
     def destroy
+        @plot.destroy
+        redirect_to plots_path 
     end
 
     private 
 
-    def world_params
-        params.require(:world).permit(
+    def plot_params
+        params.require(:plot).permit(
             :title
             :importance
             :description
