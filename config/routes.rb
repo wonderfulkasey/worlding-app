b8aeca2268435d32c38f5e2d0c2705bc7fbda494
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-22 00:44:03 -0400

    adding correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
+  resources :worlds, except: [:show]
+
+  root 'application#home'
+ 
+  #nested show url
+  get 'worlds/:world_id/characters/:character_id' 
+        =>
+  get 'worlds/:world_id/plots/:plot_id' 
+        =>
+
+  #nested new url
+  get 'worlds/:world_id/characters/new' 
+        =>
+  get 'worlds/:world_id/plots/new' 
+        =>
+
+  #form display with validations
+  #in view folder world, under form
+  
+  #custom active record scope 
+  #view worlds by most characters in world
+  get 'worlds/most-characters' => 'worlds#show'
+
+
   devise_for :users, :controllers => { registrations: "registrations",
      omniauth_callbacks: "callbacks"}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do 
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+ 
 end
